4c9f58d65253b82206e643ad886afee28306d389
StandingPad <sheikhmahid1@gmail.com>
2022-06-10 12:52:40 -0500

    In theory the spam code should be finished

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import discord
-import asyncio
+from discord.ext import tasks
 import json
 
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.spam_text = None
         self.token = None
         
+        # intents
         intents = discord.Intents.default()
         intents.message_content = True
-        
         discord.Client.__init__(self, intents=intents)
-    
+        
+        # the magic behind the whole thing
+        self.spam_text = []
+        self.reset_spam_text.start()
+        
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        if self.spam_text is not None:
-            if message.content == self.spam_text:
-                message.channel.send("no spamming!")
-        self.spam_text = message.content
-        await asyncio.sleep(100)
-        self.spam_text = None
+        if len(self.spam_text):
+            if self.spam_text.count((message.author, message.content)) > 3:
+                await message.channel.send("I said no spamming {message.author.mention}")
+                await message.guild.ban(message.author, reason="Caught spamming by helpful horse")
+                
+            if self.spam_text.count((message.author, message.content)):
+                await message.channel.send(f"No spamming {message.author.mention}")
+        self.spam_text.append((message.author, message.content)) # append the author and message
+        
+    @tasks.loop(hours=24)
+    async def reset_spam_text(self):
+        self.spam_text = []
+        
+    @reset_spam_text.before_loop
+    async def initionalize(self):
+        await self.wait_until_ready()
 
 if __name__ == "__main__":
     token = None
