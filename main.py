dc1f688098089aeb6f87ae40403790a0f774777d
StandingPad <sheikhmahid1@gmail.com>
2022-06-10 16:20:10 -0500

    Added some slash commands

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import tasks
 import json
 
-class MyClient(discord.Client):
+class MyClient(discord.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.token = None
@@ -10,7 +10,7 @@ class MyClient(discord.Client):
         # intents
         intents = discord.Intents.default()
         intents.message_content = True
-        discord.Client.__init__(self, intents=intents)
+        discord.Bot.__init__(self, intents=intents)
         
         # the magic behind the whole thing
         self.spam_text = []
@@ -37,10 +37,23 @@ class MyClient(discord.Client):
     async def initionalize(self):
         await self.wait_until_ready()
 
+client = MyClient()
+
+@client.slash_command(name="mcprep")
+async def mcprep_download(ctx):
+    await ctx.respond("MCprep can be downloaded here: https://github.com/TheDuckCow/MCprep/releases")
+    
+@client.slash_command(name="blender")
+async def blender_download(ctx):
+    await ctx.respond("Blender can be downloaded here: https://www.blender.org/")
+    
+@client.slash_command(name="assets")
+async def asset_submission(ctx):
+    await ctx.respond("You can submit mob rigs here: https://github.com/TheDuckCow/MCprep/issues/245")
+
 if __name__ == "__main__":
     token = None
     with open('config.json') as f:
         data = json.load(f)
         token = data["token"]
-    client = MyClient()
     client.run(token)
